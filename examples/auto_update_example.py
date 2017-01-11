3f7f0efeb8e12ff5dce2960109c563df070f0971
ukBaz <barry_byford@yahoo.co.uk>
2017-01-11 20:52:43 +0000

    Simplified auto update example

--- examples/auto_update_example.py
+++ examples/auto_update_example.py
@@ -2,11 +2,14 @@ from guizero import *
 import random
 
 
-def read_sensor(app_obj, text_obj):
-    sensor_value = random.randrange(3200, 5310, 10) / 100
-    text_obj.set(sensor_value)
+def read_sensor():
+    return random.randrange(3200, 5310, 10) / 100
+
+
+def update_label():
+    text.set(read_sensor())
     # recursive call
-    app_obj.update(1000, read_sensor, app_obj, text_obj)
+    text.after(1000, update_label)
 
 
 if __name__ == '__main__':
@@ -18,5 +21,5 @@ if __name__ == '__main__':
     title = Text(app, 'Sensor value:', grid=[0, 0])
     text = Text(app, "xx", grid=[1, 0])
 
-    app.update(1000, read_sensor, app, text)
+    text.after(1000, update_label)
     app.display()
