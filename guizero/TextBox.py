b26a22e2b93e0e0c4d1b31f59165dfe725628f0a
Laura <laura.sach@raspberrypi.org>
2017-11-03 09:57:02 +0000

    Add changes to TextBox

--- guizero/TextBox.py
+++ guizero/TextBox.py
@@ -1,44 +1,56 @@
 from tkinter import Entry, StringVar, END
-
 from . import utilities as utils
-    
-class TextBox(Entry):
+
+class TextBox:
 
     def __init__(self, master, text="", width=10, grid=None, align=None):
-        
+
         # Description of this object (for friendly error messages)
         self.description = "[TextBox] object with text \"" + str(text) + "\""
 
-        # Attempt to instantiate the object and raise an error if failed
-        try:
-            super().__init__(master)
-        except AttributeError:
-            utils.error_format( self.description + "\n" + 
-            "The first argument was a " + str(type(master)) +". First argument must be [App] or [Box]")         
-
         # Set up controlling string variable
-        self.string_var = StringVar()
-        self.string_var.set( str(text) )
+        self._text = StringVar()
+        self._text.set( str(text) )
 
-        # Initial config on setup  
-        self.config(textvariable=self.string_var, width=width)
-        
+        # Create a tk Label object within this object
+        self.tk = Entry(master, textvariable=self._text, width=width)
+
         # Pack or grid depending on parent
         utils.auto_pack(self, master, grid, align)
-            
-    # Clear text box 
-    def clear(self):
-        self.delete(0, END)
-    
-    # Returns the text
-    def get(self):
-        return self.string_var.get()
 
-    # Sets the StringVar
-    def set(self, text):
-        self.string_var.set( str(text) )
+
+    # PROPERTIES
+    # ----------------------------------
+    # The text value
+    @property
+    def value(self):
+        return self._text.get()
+
+    @value.setter
+    def value(self, value):
+        self._text.set( str(value) )
+        self.description = "[Text] object with text \"" + str(value) + "\""
+
+
+    # METHODS
+    # -------------------------------------------
+    # Clear text box
+    def clear(self):
+        self.tk.delete(0, END)
 
     # Append text
     def append(self, text):
-        self.string_var.set( self.string_var.get() + str(text) )
-        
+        self.value = self.value + str(text) 
+        self.description = "[Text] object with text \"" + self.value + "\""
+
+
+    # DEPRECATED METHODS
+    # --------------------------------------------
+    # Returns the text
+    def get(self):
+        return self._text.get()
+
+    # Sets the text
+    def set(self, text):
+        self._text.set( str(text) )
+        self.description = "[Text] object with text \"" + str(text) + "\""
