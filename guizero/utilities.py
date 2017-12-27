8839716fed4c44a0ba16a830749659cc081b4417
Stewart Watkiss <stewart@watkissonline.co.uk>
2017-12-27 16:21:46 +0000

    Add columnspan and rowspan option to the grid=[] feature. This uses grid=[x,y,xspan,yspan]

--- guizero/utilities.py
+++ guizero/utilities.py
@@ -9,14 +9,23 @@ def auto_pack(widget, master, grid, align):
     else:
 
         # If they failed to specify grid coords
-        if grid is None or type(grid) is not list or len(grid) != 2:
+        # Can have 2 values (just coords) or 4 values (coords and col/rowspan)
+        if grid is None or type(grid) is not list or (len(grid) != 2 and len(grid) != 4):
             error_format(widget.description + " will not be displayed because it has a missing or " +
-            "incorrect grid reference. The format should be grid=[x, y].")
+            "incorrect grid reference. The format should be grid=[x, y] or grid=[x, y, columnspan, rowspan].")
 
         else:
+            # if we have col span and row span then use them, otherwise default to 1 for both
+            columnspan = 1
+            rowspan = 1
+            # Just check we have more than 2 as we have already checked it's a multiple of two previously
+            if len(grid) > 2:
+                columnspan = grid[2]
+                rowspan = grid[3]
+            
             # If no alignment, just place in grid with center align default
             if align is None:
-                widget.tk.grid(row=grid[1], column=grid[0])
+                widget.tk.grid(row=grid[1], column=grid[0], columnspan=columnspan, rowspan=rowspan)
             else:
                 # Conversion to child friendly specifications (diags?)
                 directions = {"top": "N", "bottom": "S", "left": "W", "right": "E"}
@@ -29,7 +38,7 @@ def auto_pack(widget, master, grid, align):
                     "\nShould be: top, bottom, left or right")
 
                 # Place on grid
-                widget.tk.grid(row=grid[1], column=grid[0], sticky=align_this)
+                widget.tk.grid(row=grid[1], column=grid[0], columnspan=columnspan, rowspan=rowspan, sticky=align_this)
 
 
 # Lambda-izer for making it easy to pass arguments with function calls
