30c799049cee220eb9277a91ed6590f7d3629f42
Martin O'Hanlon <martin@ohanlonweb.com>
2018-01-02 20:52:16 +0000

    fixed widget.show

--- examples/grid_layout.py
+++ examples/grid_layout.py
@@ -1,9 +1,20 @@
-from guizero import App, PushButton
+from guizero import App, PushButton, Slider
 
 def print_pos(x, y):
     print("{}, {}".format(x, y))
-    b00.show()
 
+def hide():
+    b00.hide()
+    b01.hide()
+    b10.hide()
+    b11.hide()
+    
+def show():
+    b00.show()
+    b01.show()
+    b10.show()
+    b11.show()
+    
 app = App(layout="grid")
 
 b00 = PushButton(app, print_pos, text="0,0", args=[0,0], grid=[0,0])
@@ -11,7 +22,7 @@ b01 = PushButton(app, print_pos, text="0,1", args=[0,1], grid=[0,1])
 b10 = PushButton(app, print_pos, text="1,0", args=[1,0], grid=[1,0])
 b11 = PushButton(app, print_pos, text="1,1", args=[1,1], grid=[1,1])
 
-b00.hide()
-
+b_hide = PushButton(app, hide, text="hide", grid=[0,2])
+b_show = PushButton(app, show, text="show", grid=[1,2])
 
 app.display()
--- guizero/tkmixins.py
+++ guizero/tkmixins.py
@@ -1,3 +1,4 @@
+from . import utilities as utils
 
 class ScheduleMixin():
     _callback = {}
@@ -65,28 +66,17 @@ class FocusMixin():
         self.tk.focus_set()
 
 class DisplayMixin():
-    _display_cache = {}
-
-    def _get_display_type(self):
-        if not self.tk.grid_info():
-            return "pack"
-        return "grid"
-
+    
     def hide(self):
         """Hide the widget."""
-        display_type = self._get_display_type()
-        if display_type == "pack":
-            self.tk.pack_forget()
-        else:
+        if self.master.layout == "grid":
             self.tk.grid_forget()
+        else:
+            self.tk.pack_forget()
 
     def show(self):
         """Show the widget."""
-        display_type = self._get_display_type()
-        if display_type == "pack":
-            self.tk.pack(**self._display_cache)
-        else:
-            self.tk.grid(**self._display_cache)
+        utils.auto_pack(self, self.master, self.grid, self.align)
 
 class SizeMixin():
     @property
