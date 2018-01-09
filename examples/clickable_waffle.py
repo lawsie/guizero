8cdbe35eda079a218ca41172931a32069a69cb4b
Scott Thomson <scott1.thomson@ge.com>
2018-01-09 14:25:26 +0000

    Removing extra CR/LF

--- examples/clickable_waffle.py
+++ examples/clickable_waffle.py
@@ -13,5 +13,4 @@ app = App("Waffle!", height=50*20, width=50*20)
 # Create a waffle 80x80 with circular pixels 10x10 and 2px padding between them
 big_waffle = Waffle(app, height=80, width=80, dim=10, pad=2, dotty=True, remember=True,command=change_pixel)
 
-
 app.display()
