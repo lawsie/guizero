9c7202b13f04ca483cb693a88b644552d780020c
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-02-26 19:18:10 +0000

    refactored Picture

--- guizero/Picture.py
+++ guizero/Picture.py
@@ -1,3 +1,4 @@
+
 from tkinter import Label, PhotoImage, TclError
 from .mixins import WidgetMixin
 from .tkmixins import ScheduleMixin, DestroyMixin, EnableMixin, FocusMixin, DisplayMixin, ColorMixin, ReprMixin
@@ -27,23 +28,10 @@ class Picture(
         self._align = align
         self._visible = True
 
-        # The image name as a string
-        self._image_name = str(image)
-
-        # Description of this object (for friendly error messages)
-        self.description = "[Picture] object \"" + self._image_name + "\""
-
         # Instantiate label object which will contain image
         self.tk = Label(master.tk)
 
-        try:
-            img = PhotoImage(master=self.tk.winfo_toplevel(), file=image)
-            self._image = img
-            self.tk.config(image=self._image)
-
-        except TclError:
-            self.tk.config(text="Image "+ self._image_name +" failed to load")
-            utils.error_format("Image import error - " + str(image) +" must be a GIF, check correct path")
+        self.value = image
 
         # Pack or grid depending on parent
         utils.auto_pack(self, master, grid, align)
@@ -53,32 +41,30 @@ class Picture(
     # Get the filename of the image
     @property
     def value(self):
-        return (self._image_name)
+        return (self._image_path)
 
     # Set the image to a given file
     @value.setter
     def value(self, image):
+        self._image_path = str(image)
+        self.description = "[Picture] object \"" + self._image_path + "\""
+
         try:
             img = PhotoImage(master=self.tk.winfo_toplevel(), file=image)
+
+            # ok...  Unless self._image is set to img Picture doesnt work... I have no idea why! 
+            # There must be tkinter weirdness going on
             self._image = img
-            self.tk.config(image=self._image)
-            self._image_name = str(image)
-            self.description = "[Picture] object \"" + str(image) + "\""
+            
+            self.tk.config(image=img)
+            
         except:
             utils.error_format("Image import error - " + str(image) +" must be a GIF, check correct path")
-
 
     # DEPRECATED METHODS
     # --------------------------------------------
 
     # Sets the image to something new
     def set(self, image):
-        try:
-            img = PhotoImage(master=self.tk.winfo_toplevel(), file=image)
-            self._image = img
-            self.tk.config(image=self._image)
-            self._image_name = str(image)
-            self.description = "[Picture] object \"" + str(image) + "\""
-        except:
-            utils.error_format("Image import error - " + str(image) +" must be a GIF, check correct path")
+        self.value = image
         utils.deprecated("Picture set() is deprecated. Please use the value property instead.")
