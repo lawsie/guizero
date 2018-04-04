0303a3e473baf179634ce264ea733b41520350de
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-04-04 10:39:09 +0100

    simplified event data structure

--- examples/app_events.py
+++ examples/app_events.py
@@ -4,32 +4,36 @@ def clicked(e):
     print("clicked")
     print(e.widget)
     print(e.tk_event.type)
-    print(e.mouse_x)
-    print(e.mouse_x)
+    print(e.x)
+    print(e.y)
     print(e.key)
 
 def key(e):
     print("key pressed")
     print(e.widget)
     print(e.tk_event.type)
-    print(e.mouse_x)
-    print(e.mouse_x)
+    print(e.x)
+    print(e.y)
+    print(e.display_x)
+    print(e.display_y)
     print(e.key)
 
 def mouse_over(e):
     print("mouse over")
     print(e.widget)
     print(e.tk_event.type)
-    print(e.mouse_x)
-    print(e.mouse_x)
+    print(e.x)
+    print(e.y)
     print(e.key)
 
 def mouse_moved(e):
     print("mouse moved")
     print(e.widget)
     print(e.tk_event.type)
-    print(e.mouse_x)
-    print(e.mouse_x)
+    print(e.x)
+    print(e.y)
+    print(e.display_x)
+    print(e.display_y)
     print(e.key)
 
 def press():
@@ -49,24 +53,24 @@ text_box = TextBox(app)
 # app.when_mouse_over = mouse_over
 # app.when_mouse_moved = mouse_moved
 
-app.when_left_button_pressed = press
-app.when_left_button_released = release
+# app.when_left_button_pressed = press
+# app.when_left_button_released = release
 
-app.when_right_button_pressed = press
-app.when_right_button_released = release
+# app.when_right_button_pressed = press
+# app.when_right_button_released = release
 
-app.when_key_pressed = press
-app.when_key_released = release
+# app.when_key_pressed = press
+# app.when_key_released = release
 
 # text.when_clicked = clicked
 # text.when_key_pressed = key
 # text.when_mouse_over = mouse_over
 # text.when_mouse_moved = mouse_moved
 
-# text_box.when_clicked = clicked
-# text_box.when_key_pressed = key
-# text_box.when_mouse_over = mouse_over
-# text_box.when_mouse_moved = mouse_moved
+text_box.when_clicked = clicked
+text_box.when_key_pressed = key
+text_box.when_mouse_over = mouse_over
+text_box.when_mouse_moved = mouse_moved
 
 
 app.display()
--- guizero/event.py
+++ guizero/event.py
@@ -6,9 +6,6 @@ class EventData():
     def __init__(self, widget, tk_event):
         self._widget = widget
         self._tk_event = tk_event
-        self._char = tk_event.char
-        self._x = tk_event.x
-        self._y = tk_event.y
 
     @property
     def widget(self):
@@ -20,24 +17,23 @@ class EventData():
 
     @property
     def key(self):
-        return self._char
+        return self._tk_event.char
 
     @property
-    def mouse_x(self):
-        return self._x
+    def x(self):
+        return self._tk_event.x
 
     @property
-    def mouse_y(self):
-        return self._y
+    def y(self):
+        return self._tk_event.y
 
-    def __eq__(self, other):
-        return self.key == other
+    @property
+    def display_x(self):
+        return self._tk_event.x_root
 
-    def __lt__(self, other):
-        return self.key < other
-    
-    def __gt__(self, other):
-        return self.key > other
+    @property
+    def display_y(self):
+        return self._tk_event.y_root
 
 
 class EventCallback():
@@ -54,10 +50,8 @@ class EventCallback():
             self._callback()
         elif args_expected == 1:
             self._callback(EventData(self._widget, tk_event))
-        elif args_expected == 2:
-            self._callback(tk_event.x, tk_event.y)
         else:
-            utils.error_format("An event callback function must accept either 0, 1 or 2 arguments.\nThe current callback has {} arguments.".format(args_expected))    
+            utils.error_format("An event callback function must accept either 0 or 1 arguments.\nThe current callback has {} arguments.".format(args_expected))    
     
     def clear(self):
         self._widget.tk.unbind(self._event, self._func_id)
