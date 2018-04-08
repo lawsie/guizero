fc3f4b0068a179e2a4473e5844cc3f9d99a47569
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-04-08 08:45:26 +0100

    update events example

--- examples/app_events.py
+++ examples/app_events.py
@@ -1,124 +1,56 @@
-from guizero import App, Box, Text, TextBox, Slider, Waffle, CheckBox, Combo, Picture, ButtonGroup
+from guizero import App, Text, TextBox, Slider, Waffle, CheckBox, info
 
-def print_event_data(e):
-    print(e.tk_event.type)
-    print(e.x)
-    print(e.y)
-    print(e.display_x)
-    print(e.display_y)
-    print(e.key)
+def intro():
+    info("events", "this is all about events")
 
-def clicked(e):
-    print("clicked")
-    print_event_data(e)
-    
-def key(e):
-    print("key pressed")
-    print_event_data(e)
-    
-def mouse_over(e):
-    print("mouse over")
-    print_event_data(e)
-    
-def mouse_moved(e):
-    print("mouse moved")
-    print_event_data(e)
-    
-def mouse_dragged(e):
-    print("mouse dragged")
-    print_event_data(e)
-    
-def press(e):
-    print("press")
-    print_event_data(e)
-    e.widget.bg = "red"
+# when return is pressed back the test red and then turn it back to black
+def key_pressed(e):
 
-def release(e):
-    print("release")
-    print_event_data(e)
-    e.widget.bg = "white"
+    def text_color_black():
+        text_box.text_color = "black"
+
+    # return pressed
+    if ord(e.key) == 13:
+        e.widget.text_color = "red"
+        e.widget.after(1000, text_color_black)
+
+def mouse_enters(e):
+    e.widget.bg = "lightblue"
+
+def mouse_leaves(e):
+    e.widget.bg = "white" 
+
+def right_pressed():
+    waffle.set_pixel(0,0,"green")
+
+def right_released():
+    waffle.set_pixel(0,0,"grey")
 
 app = App()
-box = Box(app)
-text = Text(box, text="events")
-slider = Slider(box)
-check = CheckBox(app, "check")
-combo = Combo(app, ["hi", "bye"])
-pic = Picture(app, image="guizero.gif")
-waffle = Waffle(app)
+text = Text(app, text="events")
+slider = Slider(app)
 text_box = TextBox(app)
-choice = ButtonGroup(app, options=["cheese", "ham", "salad"])
+check = CheckBox(app, "check")
+waffle = Waffle(app)
 
-text_box.when_mouse_enters = press
-text_box.when_mouse_leaves = release
+# when clicked
+text.when_clicked = intro
 
-# choice.when_left_button_pressed = press
-# choice.when_left_button_released = release
-#choice._options[0].when_left_button_pressed = press
+# when key pressed
+text_box.when_key_pressed = key_pressed
 
-# slider.when_left_button_pressed = press
-# slider.when_left_button_released = release
-# slider.when_key_pressed = press
-# slider.when_key_released = release
+# highlight widget when move over
+check.when_mouse_enters = mouse_enters
+check.when_mouse_leaves = mouse_leaves
+slider.when_mouse_enters = mouse_enters
+slider.when_mouse_leaves = mouse_leaves
+text_box.when_mouse_enters = mouse_enters
+text_box.when_mouse_leaves = mouse_leaves
+waffle.when_mouse_enters = mouse_enters
+waffle.when_mouse_leaves = mouse_leaves
 
-# check.when_left_button_pressed = press
-# check.when_left_button_released = release
-# check.when_key_pressed = press
-# check.when_key_released = release
-
-# box.when_left_button_pressed = press
-# box.when_left_button_released = release
-# box.when_key_pressed = press
-# box.when_key_released = release
-
-# pic.when_left_button_pressed = press
-# pic.when_left_button_released = release
-# pic.when_key_pressed = press
-# pic.when_key_released = release
-
-
-# combo.when_left_button_pressed = press
-# combo.when_left_button_released = release
-# combo.when_key_pressed = press
-# combo.when_key_released = release
-
-
-#slider.when_clicked = clicked
-# waffle.when_clicked = clicked
-# waffle.when_key_pressed = key
-
-# waffle.when_left_button_pressed = press
-# waffle.when_left_button_released = release
-# waffle.when_mouse_enters = mouse_over
-
-# app.when_clicked = clicked
-# app.events.set_event("myclick", "<Button-1>", press)
-
-# app.when_key_pressed = key
-# app.events.set_event("myref1", "<KeyRelease>", release)
-# app.events.set_event("myref2", "<ButtonPress-1>", press)
-# app.events.set_event("myref2", "<ButtonRelease-1>", release)
-# app.when_mouse_enters = mouse_over
-# app.when_mouse_moved = mouse_moved
-
-# app.when_left_button_pressed = press
-# app.when_left_button_released = release
-
-# app.when_right_button_pressed = press
-# app.when_right_button_released = release
-
-# app.when_key_pressed = press
-# app.when_key_released = release
-
-# text.when_clicked = clicked
-# text.when_key_pressed = key
-# text.when_mouse_enters = mouse_over
-# text.when_mouse_moved = mouse_moved
-
-# text_box.when_clicked = clicked
-# text_box.when_key_pressed = key
-# text_box.when_mouse_enters = mouse_over
-# text_box.when_mouse_moved = mouse_moved
-
+# right button pressed and released
+waffle.when_right_button_pressed = right_pressed
+waffle.when_right_button_released = right_released
 
 app.display()
