749e042f8d1f5f60516659a71d33bdee293ad95b
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2018-07-17 08:02:28 +0100

    combo default test

--- tests/test_combo.py
+++ tests/test_combo.py
@@ -50,13 +50,20 @@ def test_getters_setters():
 
 def test_select_default():
     a = App()
-    c = Combo(a, ["foo", "bar"], selected="bar")
+    c1 = Combo(a, ["foo", "bar"], selected="bar")
+    c2 = Combo(a, ["foo", "bar"])
     
-    assert c.value == "bar"
-    c.value = "foo"
-    assert c.value == "foo"
-    c.select_default()
-    assert c.value == "bar"
+    assert c1.value == "bar"
+    c1.value = "foo"
+    assert c1.value == "foo"
+    c1.select_default()
+    assert c1.value == "bar"
+
+    assert c2.value == "foo"
+    c2.value = "bar"
+    assert c2.value == "bar"
+    c2.select_default()
+    assert c2.value == "foo"
 
     a.destroy()
 
