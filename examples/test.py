a0937f09b652a2ed16298d7c456d3244d02d2b17
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2019-02-05 13:28:47 +0000

    remove old files

--- examples/test.py
+++
@@ -1,8 +0,0 @@
-from guizero import App, Box, PushButton
-app = App()
-
-buttons_box = Box(app, width="fill", align="bottom")
-ok = PushButton(buttons_box, text="OK", align="right")
-cancel = PushButton(buttons_box, text="Cancel", align="right")
-
-app.display()
