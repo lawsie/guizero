0f82aa621dd1ec57895a2e2636eb2865a7147c69
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2019-02-24 15:57:33 +0000

    added rectangle and circle

--- examples/drawing.py
+++ examples/drawing.py
@@ -1,6 +1,15 @@
-from guizero import App, Drawing
+from guizero import App, Drawing, PushButton
 
 a = App()
-d = Drawing(a)
-d.tk.create_rectangle(0,0,50,50,fill="blue")
+
+# create drawing object
+d = Drawing(a, width="fill", height="fill")
+d.bg = "light blue"
+
+# drawing the shapes
+d.rectangle(10, 10, 200, 200, width=10, fill_color="grey")
+d.circle(50, 50, 15, fill_color="blue")
+d.circle(150, 50, 15, fill_color="blue")
+d.rectangle(20,160,190,170, color="red", fill_color="black")
+
 a.display()
--- guizero/Drawing.py
+++ guizero/Drawing.py
@@ -1,4 +1,4 @@
-from tkinter import Canvas, BOTH, Frame
+from tkinter import Canvas, BOTH, ALL
 from . import utilities as utils
 from .base import Widget
 from .event import EventManager
@@ -13,4 +13,24 @@ class Drawing(Widget):
 
         super(Drawing, self).__init__(master, tk, description, grid, align, visible, enabled, width, height)
 
+    def rectangle(self, x1, y1, x2, y2, width=1, color="black", fill_color=None):
 
+        self.tk.create_rectangle(
+            x1, y1, x2, y2, 
+            outline = "" if color is None else color,
+            width = 0 if width is None else width,
+            fill = "" if fill_color is None else fill_color
+            )
+
+    def circle(self, x, y, radius, width=1, color="black", fill_color=None):
+        
+        self.tk.create_oval(
+            x - radius, y - radius, 
+            x + radius, y + radius,
+            outline = "" if color is None else color,
+            width = 0 if width is None else width,
+            fill = "" if fill_color is None else fill_color
+            )
+
+    def clear(self):
+        self.tk.delete(ALL)
