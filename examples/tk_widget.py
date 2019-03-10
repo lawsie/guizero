d08d73ed8777810ed18a6fa9f69bb48536c6ead1
Martin O'Hanlon <martin.ohanlon@raspberrypi.org>
2019-03-10 21:49:20 +0000

    update example

--- examples/tk_widget.py
+++ examples/tk_widget.py
@@ -1,17 +1,26 @@
 from guizero import App, Text
-from tkinter import Label, Entry
+from tkinter import Label, Entry, Spinbox
+from tkinter.ttk import Progressbar
 
 a = App()
+a.bg = "light blue"
+
 text_1 = Text(a, text="text 1")
 
 label = Label(a.tk, text="label")
-a.add_tk_widget(entry)
+a.add_tk_widget(label)
 
 entry = Entry(a.tk)
 guizero_entry = a.add_tk_widget(entry, height=10, width=60, enabled=False, visible=False)
-
 a.after(1000, guizero_entry.show)
-a.add_tk_widget(label)
+
+sp = Spinbox(from_=0, to=10)
+a.add_tk_widget(sp)
+
+pb = Progressbar()
+a.add_tk_widget(pb)
+pb.start()
+
 text_2 = Text(a, text="text 2")
 
 a.display()
