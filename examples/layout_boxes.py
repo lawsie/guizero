badfdc4e079db84d165e9838c446f913bfabea45
Martin O'Hanlon <martin@ohanlonweb.com>
2019-08-29 13:59:33 +0200

    updated example

--- examples/layout_boxes.py
+++ examples/layout_boxes.py
@@ -1,13 +1,6 @@
 from guizero import App, Box, Text, TextBox
 app = App()
 
-def reallign():
-    f.grid[1] = 1
-    l.grid = [0,0]
-    buttons_box.align="top"
-    title_box.align="bottom"
-    f.align = "left"
-
 title_box = Box(app, width="fill", align="top", border=True)
 Text(title_box, text="title")
 
@@ -21,10 +14,8 @@ content_box = Box(app, align="top", width="fill", border=True)
 Text(content_box, text="content")
 
 form_box = Box(content_box, layout="grid", width="fill", align="left", border=True)
-f = Text(form_box, grid=[0,0], text="form", align="right")
-l = Text(form_box, grid=[0,1], text="label        x")
+Text(form_box, grid=[0,0], text="form", align="right")
+Text(form_box, grid=[0,1], text="label", align="left")
 TextBox(form_box, grid=[1,1], text="data", width="fill")
-
-app.after(1000, reallign)
 
 app.display()
