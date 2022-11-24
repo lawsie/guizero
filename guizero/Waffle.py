76623fe60c4fa49343ca916e8af32866e8c7c541
Laura Sach <laura.sach@raspberrypi.org>
2022-11-24 14:21:59 +0000

    Don't destroy canvas

--- guizero/Waffle.py
+++ guizero/Waffle.py
@@ -60,27 +60,26 @@ class Waffle(Widget):
         self._draw_waffle()
 
     def _create_canvas(self):
-        # if the canvas exists, clear it and destroy it
+        # If the canvas exists, clear it 
         if self._canvas:
             self._canvas.delete("all")
-            self._canvas.destroy()
+        
+        # If not, make one
+        else:
 
-        # size the canvas. Each element of the waffle has padding to top,
-        # bottom, left and right, but the padding is shared between elements.
-        # What this means in pratice is that elements at the edge have a whole
-        # padding width between them and the edge of the canvas and a single
-        # padding width between individual elements, so the padding between
-        # elements is shared.
-        self._c_height = (self._height * (self._pixel_size + self._pad)) + self._pad
-        self._c_width = (self._width * (self._pixel_size + self._pad)) + self._pad
+            # size the canvas. Each element of the waffle has padding to top,
+            # bottom, left and right, but the padding is shared between elements.
+            # What this means in pratice is that elements at the edge have a whole
+            # padding width between them and the edge of the canvas and a single
+            # padding width between individual elements, so the padding between
+            # elements is shared.
+            self._c_height = (self._height * (self._pixel_size + self._pad)) + self._pad
+            self._c_width = (self._width * (self._pixel_size + self._pad)) + self._pad
 
-        # create the canvas and pack it into the waffle frame
-        self._canvas = Canvas(self.tk, height=self._c_height, width=self._c_width, bd=0, highlightthickness=0)
-        self._canvas.pack(fill=BOTH, expand=1)
+            # create the canvas and pack it into the waffle frame
+            self._canvas = Canvas(self.tk, height=self._c_height, width=self._c_width, bd=0, highlightthickness=0)
+            self._canvas.pack(fill=BOTH, expand=1)
 
-        # rebind any events as they would have been lost when the canvas
-        # was destroyed
-        self.events.rebind_events(self._canvas)
 
         # fill the canvas background
         self._canvas.create_rectangle(0, 0, self._c_width, self._c_height, fill=self.bg, outline=self.bg)
