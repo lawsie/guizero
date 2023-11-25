f93acf0b569c90c36af50e4304bffa845f4110f8
Martin O'Hanlon <martin@ohanlonweb.com>
2023-11-25 12:34:47 +0000

    uplift python version

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ __classifiers__ = [
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
     "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
+    "Programming Language :: Python :: 3.12",
 ]
 __long_description__ = """# guizero
 
